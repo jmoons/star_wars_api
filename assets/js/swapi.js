ea506abbec2f72343936a68cdc8d348edc6ffeec
John Moons <j.moons@realdigitalmedia.com>
2017-05-01 17:43:25 -0400

    Now displaying JSON data somewhat styled. I had to shift to building up DOM items to append in strings rather than piece-meal appending due to odd rendering bug observed in Chrome and Firefox.
    It appears while in a iterator loop (forEach), a collection of list items were being appended before the ul element had been appended resulting in a failure to append. My theory is non-deterministically, asking jQuery to append a li was happening before jQuery had completed the append call to add the ul element to the DOM.

--- assets/js/swapi.js
+++ assets/js/swapi.js
@@ -18,23 +18,39 @@ $(document).ready(function() {
 
 var SWAPIDemo = (function() {
 
-  var populate_active_tab = function() {
-    var active_tab = $(".tab-content .active")
-    var url_to_get = active_tab.data("url");
+  var populate_active_tab = function(data_from_server, $active_tab) {
 
-    // Clear out the tab
-    active_tab.empty();
+    data_from_server["results"].forEach( function(result, index) {
+      var panel_to_append = "";
 
-    $.ajax({
-      url: url_to_get,
-      type: "GET",
-      dataType: "json"
-    }).done(function (data_from_server) {
-      active_tab.append(JSON.stringify(data_from_server));
-    }).fail(function () {
-      console.log("SWAPI GET Failed");
-      active_tab.append("SWAPI GET Failed!");
+      // Grrrr, the flims collection uses 'title' whereas all other collections use 'name'
+      var title_of_panel = result["name"] ? result["name"] : result["title"];
+
+      // Create the Panel for this result
+      panel_to_append += '<div class="panel panel-default" id="result_panel"><div class="panel-heading"><h3 class="panel-title">' + title_of_panel + '</h3></div><div class="panel-body"><ul>'
+
+      Object.keys(result).forEach( function(key, index) {
+        if ( key != "name" && key != "title" ) {
+          if ( Array.isArray(result[key]) ) {
+            // The value is an array of links, lets just create a-refs displaying their respective IDs
+            panel_to_append += ('<li>' + key + ': ')
+            result[key].forEach( function(link_url, index) {
+              panel_to_append += ('<a href="' + link_url + '" target="_blank">' + /\d+/.exec(link_url) + '</a>, ');
+            });
+            // This will slice off the ', ' comma-space from the last list item.
+            panel_to_append = panel_to_append.slice(0, -2);
+            panel_to_append += '</li>'
+          } else {
+            panel_to_append += ('<li>' + key + ': ' + result[key] + '</li>');
+          };
+
+        };
+      });
+      panel_to_append += '</ul></div></div>';
+      $active_tab.append(panel_to_append);
+
     });
+
   }
 
   return {
@@ -46,15 +62,27 @@ var SWAPIDemo = (function() {
         $("#navbar").append('<li> <a href="#swapi_' + index_plus_one + '" data-toggle="tab">' + key + '</a></li>');
         $(".tab-content").append('<div class="tab-pane" id=swapi_' + index_plus_one + ' data-url="' + SWAPIDemo.data_structure[key] + '"></div>');
       });
-      $("#navbar li:first").addClass("active");
-      $(".tab-content div:first").addClass("active");
-
-      populate_active_tab();
 
     },
 
     handle_navbar_click: function() {
-      populate_active_tab();
+      var $active_tab = $(".tab-content .active");
+      var url_to_get = $active_tab.data("url");
+
+      // Lets keep the GETs to only if we need them
+      if ( $active_tab.children().length == 0 ) {
+        $.ajax({
+          url: url_to_get,
+          type: "GET",
+          dataType: "json"
+        }).done(function (data_from_server) {
+          populate_active_tab(data_from_server, $active_tab);
+
+        }).fail(function () {
+          console.log("SWAPI GET Failed");
+          $active_tab.append("SWAPI GET Failed!");
+        });
+      };
     }
   }
 })();
